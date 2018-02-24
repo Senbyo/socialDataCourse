acdebc149d989e3370566dc3f4a11d02809de207
Desktop/Georg <georg.h.k@hotmail.com>
2018-02-24 14:15:12 +0100

    Added labels/legends/symbols

--- Assignment1Marathon/plot2.js
+++ Assignment1Marathon/plot2.js
@@ -1,6 +1,6 @@
-var margin = {top: 50, right: 50, bottom: 60, left: 60},
-    width = 700 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+var margin = {top: 50, right: 100, bottom: 100, left: 60},
+    width = 600 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
 
 /*
  * value accessor - returns the value to encode for a given data object.
@@ -64,25 +64,29 @@ d3.csv("processed_men.csv", function(error, data) {
   svg.append("g")
       .attr("class", "x axis")
       .attr("transform", "translate(0," + height + ")")
-      .call(xAxis)
-    .append("text")
-      .attr("class", "label")
-      .attr("x", width)
-      .attr("y", -6)
-      .style("text-anchor", "end")
-      .text("Year");
+      .call(xAxis);
+ //x axis labels
+    svg.append("text")
+    .attr("transform",
+          "translate(" + (width/2) + " ," +
+                         (height + margin.top) + ")")
+    .style("text-anchor", "middle")
+    .text("Year");
 
   // y-axis
   svg.append("g")
       .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("class", "label")
+      .call(yAxis);
+
+      // text label for the y axis
+  svg.append("text")
       .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Time(minutes)");
+      .attr("y", 0 - margin.left)
+      .attr("x",0 - (height / 2))
+      .attr("dy", "1em")
+      .style("text-anchor", "middle")
+      .text("Time (minutes)");
+
 
   // draw dots
   svg.selectAll(".dot")
@@ -97,10 +101,9 @@ d3.csv("processed_men.csv", function(error, data) {
           tooltip.transition()
                .duration(200)
                .style("opacity", .9);
-          tooltip.html(d["Year"] + "<br/> (" + xValue(d)
-	        + ", " + yValue(d) + ")")
+               tooltip.html("("+d["Year"] + ","+d["Time"]+")" )
                .style("left", (d3.event.pageX + 5) + "px")
-               .style("top", (d3.event.pageY - 28) + "px");
+               .style("top", (d3.event.pageY + 20) + "px");
       })
       .on("mouseout", function(d) {
           tooltip.transition()
@@ -109,30 +112,24 @@ d3.csv("processed_men.csv", function(error, data) {
       });
 
 
-  // draw legend
-  var legend = svg.selectAll(".legend")
-    .data(color.domain())
-    .enter().append("g")
-      .attr("class", "legend")
-      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
-
-
 			svg.selectAll()
 				.data(data2)
-				.enter().append("circle")
+				.enter().append("rect")
 					.attr("class", "dot")
-					.attr("r", 3.5)
-					.attr("cx", xMap)
-					.attr("cy", yMap)
-					.style("fill", function(d) { return "red";})
+          .attr("x", xMap)         // position the left of the rectangle
+          .attr("y", yMap)          // position the top of the rectangle
+          .attr("height", 6)    // set the height
+          .attr("width", 6)    // set the width
+          .attr("rx", 2)         // set the x corner curve radius
+          .attr("ry", 1)       // set the y corner curve radius
+					.style("fill", function(d) {  return color(cValue("Women"));})
 					.on("mouseover", function(d) {
 							tooltip.transition()
 									 .duration(200)
 									 .style("opacity", .9);
-							tooltip.html(d["Year"] + "<br/> (" + xValue(d)
-							+ ", " + yValue(d) + ")")
+							tooltip.html("("+d["Year"] + ","+d["Time"]+")" )
 									 .style("left", (d3.event.pageX + 5) + "px")
-									 .style("top", (d3.event.pageY - 28) + "px");
+									 .style("top", (d3.event.pageY - 20) + "px");
 					})
 					.on("mouseout", function(d) {
 							tooltip.transition()
@@ -140,11 +137,47 @@ d3.csv("processed_men.csv", function(error, data) {
 									 .style("opacity", 0);
 					});
 
-          var lineFunction = d3.line()
+           var lineFunction = d3.line()
                          .x(function(d) { return d.x; })
                          .y(function(d) { return d.y; });
 
-          //line slope and intercept
+
+           var legendRectSize = 18;                                  // NEW
+           var legendSpacing = 4;                                    // NEW
+
+
+           // draw legend for men
+           var legend = svg.selectAll('.legend')                     // NEW
+                 .data(color.domain())                                   // NEW
+                 .enter()                                                // NEW
+                 .append('g')                                            // NEW
+                 .attr('class', 'legend')                                // NEW
+                 .attr('transform', function(d, i) {                     // NEW
+                   var height = legendRectSize + legendSpacing;          // NEW
+                   var offset =  height * color.domain().length / 2;     // NEW
+                   var horz = 25 * legendRectSize;                       // NEW
+                   var vert = i * height - offset;                       // NEW
+                   return 'translate(' + horz + ',' + vert + ')';        // NEW
+                 });                                                     // NEW
+
+
+               legend.append('rect')                                     // NEW
+                 .attr('width', legendRectSize)                          // NEW
+                 .attr('height', legendRectSize)                         // NEW
+                 .style('fill', color)                                   // NEW
+                 .style('stroke', color);                                // NEW
+
+               legend.append('text')                                     // NEW
+                 .attr('x', legendRectSize + legendSpacing)              // NEW
+                 .attr('y', legendRectSize - legendSpacing)              // NEW
+                 .text(function(d) { return d; });                       // NEW
+                                                      // NEW
+
+
+//TODO:
+////////LINES.. NOT WORKING RIGHT NOW ///////////////////
+
+//line slope and intercept
           var aMen = -0.29;
           var bMen = 709.71;
 
@@ -152,19 +185,24 @@ d3.csv("processed_men.csv", function(error, data) {
           var bWomen=2018.72;
 
           var lineData = [ { "x": 1900,   "y":aMen * 1900 +bMen },  { "x": 2013,  "y": aMen * 2013 +bMen}];
+          // define the line
+          var valueline = d3.line()
+              .x(function(d) { return d.x; })
+              .y(function(d) { return d.y; });
+//aMen * 1900 +bMen
+//aMen * 2013 +bMen
 
-          svg.append("line")
-          					.attr("class", "line safeLevel")
-          					.attr("x1", 1900)
-          					.attr("x2", 2013)
-          					.attr("y1", aMen * 1900 +bMen)
-          					.attr("y2", aMen * 2013 +bMen);
+    //  svg.append("line")          // attach a line
+  //    .style("stroke", "black")  // colour the line
+  //  .attr("x1", 100)     // x position of the first end of the line
+  //    .attr("y1", 100)      // y position of the first end of the line
+  //    .attr("x2", 200)     // x position of the second end of the line
+  //    .attr("y2", 200);    // y position of the second end of the line
 
-//The line SVG Path we draw
-          var lineGraph = svg.append("path")
-                            .attr("d", lineFunction(lineData))
-                            .attr("stroke", "blue")
-                            .attr("stroke-width", 2)
-                            .attr("fill", "none");
+      // Add the valueline path.
+      svg.append("path")
+            .data(lineData)
+            .attr("class", "line")
+            .attr("d", valueline);
 
 })});
