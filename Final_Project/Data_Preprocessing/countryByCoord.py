f21fbb34a1a548103b878287a0b4330ce1d6e5f5
Desktop/Georg <georg.h.k@hotmail.com>
2018-04-27 17:01:32 +0200

    CurrentCountry working

--- Final_Project/Data_Preprocessing/countryByCoord.py
+++ Final_Project/Data_Preprocessing/countryByCoord.py
@@ -1,12 +1,13 @@
 import copyshapes
 import countries
 
-def getCoord(coord):
+def getCountry(coord):
     copyshapes.filter_file(
             lambda x: x.GetField('REGION') == 150,
             'TM_WORLD_BORDERS-0.3.shp', 'EUROPE.shp')
     
     cc = countries.CountryChecker('TM_WORLD_BORDERS-0.3.shp')
-    return cc.getCountry(countries.Point(coord[0], coord[1]))
+    country = cc.getCountry(countries.Point(coord[0], coord[1]))
+    return country
 
 
--- Final_Project/Data_Preprocessing/preprocessingScript.py
+++ Final_Project/Data_Preprocessing/preprocessingScript.py
@@ -9,38 +9,41 @@ warnings.filterwarnings('ignore')
 from scipy.misc import imread
 from subprocess import check_output
 import datetime
-from countryByCoord import getCoord
+from countryByCoord import getCountry
 
-terror=pd.read_csv('globalterrorismdb_0617dist.csv',encoding='ISO-8859-1')
-terror.rename(columns={'iyear':'Year','imonth':'Month','iday':'Day','country_txt':'Country','region_txt':'Region','attacktype1_txt':'AttackType','target1':'Target','nkill':'Killed','nwound':'Wounded','summary':'Summary','gname':'Group','targtype1_txt':'Target_type','weaptype1_txt':'Weapon_type','motive':'Motive','city':'City','latitude':'Latitude', 'longitude':'Longitude'},inplace=True)
-terror=terror[['Year','Month','Day','Country','Region','City','Latitude','Longitude','AttackType','Killed','Wounded','Target','Summary','Group','Target_type','Weapon_type','Motive']]
-terror['Victims']=terror['Killed']+terror['Wounded']
+imported = True
+if(imported):
+    terror=pd.read_csv('globalterrorismdb_0617dist.csv',encoding='ISO-8859-1')
+    terror.rename(columns={'iyear':'Year','imonth':'Month','iday':'Day','country_txt':'Country','region_txt':'Region','attacktype1_txt':'AttackType','target1':'Target','nkill':'Killed','nwound':'Wounded','summary':'Summary','gname':'Group','targtype1_txt':'Target_type','weaptype1_txt':'Weapon_type','motive':'Motive','city':'City','latitude':'Latitude', 'longitude':'Longitude'},inplace=True)
+    terror=terror[['Year','Month','Day','Country','Region','City','Latitude','Longitude','AttackType','Killed','Wounded','Target','Summary','Group','Target_type','Weapon_type','Motive']]
+    terror['Victims']=terror['Killed']+terror['Wounded']
+    
+    terror.isnull().sum()
+    terror = terror.dropna(axis=0, how = 'any',subset = ['Longitude'])
+    terror = terror.dropna(axis=0, how = 'any',subset = ['Latitude'])
+    
+    #make feature datetime object
+    terror["Month"] = terror["Month"].replace(0,1)
+    terror["Day"] = terror["Day"].replace(0,1)
+    terror["Year"] = terror["Year"].replace(0,1)
+    
+    terror['Date'] = terror[['Year', 'Month', 'Day']].apply(lambda s : datetime.datetime(*s),axis = 1)
+    terror=terror[['Date','Country','Region','City','Latitude','Longitude','AttackType','Victims','Killed','Target','Summary','Group','Target_type','Weapon_type','Motive','Year','Month','Day']]
+    terror['Date'] = terror['Date'].dt.date
+    
+    terror["AttackType"] = terror["AttackType"].replace("Hostage Taking (Kidnapping)","Kidnapping")
+    terror["AttackType"] = terror["AttackType"].replace("Hostage Taking (Barricade Incident)","Hostage Taking")
+    terror["AttackType"] = terror["AttackType"].replace("Facility/Infrastructure Attack","Infrastructure Attack")
 
-terror.isnull().sum()
-terror = terror.dropna(axis=0, how = 'any',subset = ['Longitude'])
-terror = terror.dropna(axis=0, how = 'any',subset = ['Latitude'])
-
-#make feature datetime object
-terror["Month"] = terror["Month"].replace(0,1)
-terror["Day"] = terror["Day"].replace(0,1)
-terror["Year"] = terror["Year"].replace(0,1)
-
-terror['Date'] = terror[['Year', 'Month', 'Day']].apply(lambda s : datetime.datetime(*s),axis = 1)
-terror=terror[['Date','Country','Region','City','Latitude','Longitude','AttackType','Victims','Killed','Target','Summary','Group','Target_type','Weapon_type','Motive','Year','Month','Day']]
-terror['Date'] = terror['Date'].dt.date
-
-terror["AttackType"] = terror["AttackType"].replace("Hostage Taking (Kidnapping)","Kidnapping")
-terror["AttackType"] = terror["AttackType"].replace("Hostage Taking (Barricade Incident)","Hostage Taking")
-terror["AttackType"] = terror["AttackType"].replace("Facility/Infrastructure Attack","Infrastructure Attack")
-
-terror = terror.sort_values(['Date'])
-terror.to_csv("terror_all_processed_data.csv",index=False)
+    terror = terror.sort_values(['Date'])
+    #terror.to_csv("terror_all_processed_data.csv",index=False)
 
 
 #change old countries
-
-
-
-#make a subset for europe
 terrorEU = terror[terror['Region'].str.contains("Europe")]
-terrorEU.to_csv("terror_EU_processed_data.csv",index=False)
+terrorEU["CurrentCountry"] = "none"
+for i in range(len(terrorEU['Country'])):
+    terrorEU.iloc[i,-1] = getCountry([terrorEU.iloc[i,4],terrorEU.iloc[i,5]])
+    
+#make a subset for europe
+terrorEU.to_csv("terror_EU_processed_data_updated.csv",index=False)
