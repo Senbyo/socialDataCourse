da2acf1bc214c0972e79fa8438dd99a3d2980bd4
Georg <s170258@student.dtu.dk>
2018-05-07 16:48:22 +0200

    Ongoing work on the explainer page

---
+++ ProjectB/data/loadAndTakeMetrics.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+
+terrorEU=pd.read_csv('terror_EU_processed_data.csv',encoding='latin-1')
+features = np.array(np.array(terrorEU.columns).tolist())
+
+terrorEU["AttackType"].value_counts().index
+terrorEU["Group"].loc[terrorEU["Year"]==2016].value_counts()
+ISIS = terrorEU["Group"].loc[(terrorEU["Group"]=="Islamic State of Iraq and the Levant (ISIL)") |  (terrorEU["Group"]=="Jihadi-inspired extremists")]
+ISIS.count()
