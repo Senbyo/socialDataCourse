e29e824779aa6a018b00a59a44d7d326ac78be1d
Mark Bo Jensen <mboje@win.dtu.dk>
2020-01-29 15:34:00 +0100

    testing and updating

--- teaching/vertex_demo.js
+++ teaching/vertex_demo.js
@@ -183,11 +183,6 @@ function drawScene(gl, programInfo, buffers) {
                      aspect,
                      zNear,
                      zFar);
-
-    mat4.rotate(modelViewMatrix,  // destination matrix
-                    modelViewMatrix,  // matrix to rotate
-                    45.0,   // amount to rotate in radians
-                    [0, 1, 0]);       // axis to rotate around
   
     // Set the drawing position to the "identity" point, which is
     // the center of the scene.
@@ -199,6 +194,11 @@ function drawScene(gl, programInfo, buffers) {
     mat4.translate(modelViewMatrix,     // destination matrix
                    modelViewMatrix,     // matrix to translate
                    [-0.0, 0.0, -6.0]);  // amount to translate
+
+    mat4.rotate(modelViewMatrix,  // destination matrix
+                    modelViewMatrix,  // matrix to rotate
+                    45.0,   // amount to rotate in radians
+                    [0, 1, 0]);       // axis to rotate around
   
     // Tell WebGL how to pull out the positions from the position
     // buffer into the vertexPosition attribute.
