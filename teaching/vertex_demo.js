77d4a0273ffdc53fab29d099c09cb485017cd4b2
Mark Bo Jensen <mboje@win.dtu.dk>
2020-01-29 15:01:56 +0100

    testing and updating

--- teaching/vertex_demo.js
+++ teaching/vertex_demo.js
@@ -154,7 +154,7 @@ function initShaderProgram(gl, vsSource, fsSource) {
 
 
 function drawScene(gl, programInfo, buffers) {
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
+    gl.clearColor(0.3921, 0.5843, 0.9294, 1.0);  // Clear to black, fully opaque
     gl.clearDepth(1.0);                 // Clear everything
     gl.enable(gl.DEPTH_TEST);           // Enable depth testing
     gl.depthFunc(gl.LEQUAL);            // Near things obscure far things
