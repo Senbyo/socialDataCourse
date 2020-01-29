c3c8a1094858e662201e7fb5dcaf1a79e16ce1ce
Mark Bo Jensen <mboje@win.dtu.dk>
2020-01-29 16:04:53 +0100

    testing and updating

--- teaching/vertex_demo.js
+++ teaching/vertex_demo.js
@@ -350,13 +350,13 @@ function drawScene(gl, programInfo, buffers, Buffers_plane) {
     }
 
     {
-        const numComponents = 2;  // pull out 2 values per iteration
+        const numComponents = 3;  // pull out 2 values per iteration
         const type = gl.FLOAT;    // the data in the buffer is 32bit floats
         const normalize = false;  // don't normalize
         const stride = 0;         // how many bytes to get from one set of values to the next
                                   // 0 = use type and numComponents above
         const offset = 0;         // how many bytes inside the buffer to start from
-        gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
+        gl.bindBuffer(gl.ARRAY_BUFFER, buffers_plane.position);
         gl.vertexAttribPointer(
             programInfo.attribLocations.vertexPosition,
             numComponents,
