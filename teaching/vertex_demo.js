f713de5ac523a9f9c1bf07b300fe0d9fa62dac14
Mark Bo Jensen <mboje@win.dtu.dk>
2020-01-30 18:36:09 +0100

    updating and testin

--- teaching/vertex_demo.js
+++ teaching/vertex_demo.js
@@ -417,7 +417,7 @@ function drawScene(gl, programInfo, buffers, buffers_plane) {
     const viewMatrix = mat4.create();
 
     mat4.lookAt(viewMatrix,
-                [-0.0, 0.0, -6.0],
+                [-1.0, 1.0, -6.0],
                 [0.0, 0.0, 0.0],
                 [0.0, 1.0, 0.0]);
 
