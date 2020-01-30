621985d32faadce4fb86169122e88c6a23a9489b
Mark Bo Jensen <mboje@win.dtu.dk>
2020-01-30 16:26:07 +0100

    testing and updating

--- teaching/vertex_demo.js
+++ teaching/vertex_demo.js
@@ -16,23 +16,64 @@ function main() {
 
   // Vertex shader program
 
+//   const vsSource = `
+//     attribute vec4 aVertexPosition;
+
+//     uniform mat4 uModelViewMatrix;
+//     uniform mat4 uProjectionMatrix;
+
+//     void main() {
+//       gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+//     }
+//   `;
+
   const vsSource = `
-    attribute vec4 aVertexPosition;
+  attribute vec4 aVertexPosition;
+  attribute vec3 aVertexNormal;
+  attribute vec2 aTextureCoord;
 
-    uniform mat4 uModelViewMatrix;
-    uniform mat4 uProjectionMatrix;
+  uniform mat4 uNormalMatrix;
+  uniform mat4 uModelViewMatrix;
+  uniform mat4 uProjectionMatrix;
 
-    void main() {
-      gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
-    }
-  `;
+  varying highp vec2 vTextureCoord;
+  varying highp vec3 vLighting;
+
+  void main(void) {
+    gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+    vTextureCoord = aTextureCoord;
+
+    // Apply lighting effect
+
+    highp vec3 ambientLight = vec3(0.3, 0.3, 0.3);
+    highp vec3 directionalLightColor = vec3(1, 1, 1);
+    highp vec3 directionalVector = normalize(vec3(0.85, 0.8, 0.75));
+
+    highp vec4 transformedNormal = uNormalMatrix * vec4(aVertexNormal, 1.0);
+
+    highp float directional = max(dot(transformedNormal.xyz, directionalVector), 0.0);
+    vLighting = ambientLight + (directionalLightColor * directional);
+  }
+`;
   // Fragment shader program
 
+//   const fsSource = `
+//     void main() {
+//       gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);
+//     }
+//   `;
+
   const fsSource = `
-    void main() {
-      gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);
-    }
-  `;
+  varying highp vec3 vLighting;
+
+  uniform sampler2D uSampler;
+
+  void main(void) {
+    highp vec4 texelColor = vec4(0.0, 1.0, 0.0, 1.0);
+
+    gl_FragColor = vec4(texelColor.rgb * vLighting, texelColor.a);
+  }
+`;
 
   // Initialize a shader program; this is where all the lighting
   // for the vertices and so forth is established.
@@ -172,6 +213,52 @@ function initBuffers(gl) {
     new Float32Array(positions),
     gl.STATIC_DRAW);
 
+    // normals for the cube
+
+    const normalBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
+
+  const vertexNormals = [
+    // Front
+     0.0,  0.0,  1.0,
+     0.0,  0.0,  1.0,
+     0.0,  0.0,  1.0,
+     0.0,  0.0,  1.0,
+
+    // Back
+     0.0,  0.0, -1.0,
+     0.0,  0.0, -1.0,
+     0.0,  0.0, -1.0,
+     0.0,  0.0, -1.0,
+
+    // Top
+     0.0,  1.0,  0.0,
+     0.0,  1.0,  0.0,
+     0.0,  1.0,  0.0,
+     0.0,  1.0,  0.0,
+
+    // Bottom
+     0.0, -1.0,  0.0,
+     0.0, -1.0,  0.0,
+     0.0, -1.0,  0.0,
+     0.0, -1.0,  0.0,
+
+    // Right
+     1.0,  0.0,  0.0,
+     1.0,  0.0,  0.0,
+     1.0,  0.0,  0.0,
+     1.0,  0.0,  0.0,
+
+    // Left
+    -1.0,  0.0,  0.0,
+    -1.0,  0.0,  0.0,
+    -1.0,  0.0,  0.0,
+    -1.0,  0.0,  0.0
+  ];
+
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexNormals),
+                gl.STATIC_DRAW);
+
 
     const indexBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
@@ -196,6 +283,7 @@ function initBuffers(gl) {
 
     return {
     position: positionBuffer,
+    normal: normalBuffer,
     indices: indexBuffer,
     };
   }
@@ -319,6 +407,26 @@ function drawScene(gl, programInfo, buffers, buffers_plane) {
           programInfo.attribLocations.vertexPosition);
     }
 
+    // Tell WebGL how to pull out the normals from
+    // the normal buffer into the vertexNormal attribute.
+    {
+        const numComponents = 3;
+        const type = gl.FLOAT;
+        const normalize = false;
+        const stride = 0;
+        const offset = 0;
+        gl.bindBuffer(gl.ARRAY_BUFFER, buffers.normal);
+        gl.vertexAttribPointer(
+            programInfo.attribLocations.vertexNormal,
+            numComponents,
+            type,
+            normalize,
+            stride,
+            offset);
+        gl.enableVertexAttribArray(
+            programInfo.attribLocations.vertexNormal);
+    }
+
     // Tell WebGL which indices to use to index the vertices
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffers.indices);
 
@@ -328,6 +436,16 @@ function drawScene(gl, programInfo, buffers, buffers_plane) {
     gl.useProgram(programInfo.program);
   
     // Set the shader uniforms
+
+    const normalMatrix = mat4.create();
+    mat4.invert(normalMatrix, modelViewMatrix);
+    mat4.transpose(normalMatrix, normalMatrix);
+
+
+    gl.uniformMatrix4fv(
+        programInfo.uniformLocations.normalMatrix,
+        false,
+        normalMatrix);
   
     gl.uniformMatrix4fv(
         programInfo.uniformLocations.projectionMatrix,
