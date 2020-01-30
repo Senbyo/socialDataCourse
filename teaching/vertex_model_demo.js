acb47e5d5e4eec32f620f740fbd756e0adf80dd3
Mark Bo Jensen <mboje@win.dtu.dk>
2020-01-30 19:26:14 +0100

    updating and testing

--- teaching/vertex_model_demo.js
+++ teaching/vertex_model_demo.js
@@ -561,53 +561,53 @@ function drawScene(gl, programInfo, buffers, buffers_plane) {
 
     // Set up plane
 
-    {
-        const numComponents = 3;  // pull out 2 values per iteration
-        const type = gl.FLOAT;    // the data in the buffer is 32bit floats
-        const normalize = false;  // don't normalize
-        const stride = 0;         // how many bytes to get from one set of values to the next
-                                  // 0 = use type and numComponents above
-        const offset = 0;         // how many bytes inside the buffer to start from
-        gl.bindBuffer(gl.ARRAY_BUFFER, buffers_plane.position);
-        gl.vertexAttribPointer(
-            programInfo.attribLocations.vertexPosition,
-            numComponents,
-            type,
-            normalize,
-            stride,
-            offset);
-        gl.enableVertexAttribArray(
-            programInfo.attribLocations.vertexPosition);
-    }
+    // {
+    //     const numComponents = 3;  // pull out 2 values per iteration
+    //     const type = gl.FLOAT;    // the data in the buffer is 32bit floats
+    //     const normalize = false;  // don't normalize
+    //     const stride = 0;         // how many bytes to get from one set of values to the next
+    //                               // 0 = use type and numComponents above
+    //     const offset = 0;         // how many bytes inside the buffer to start from
+    //     gl.bindBuffer(gl.ARRAY_BUFFER, buffers_plane.position);
+    //     gl.vertexAttribPointer(
+    //         programInfo.attribLocations.vertexPosition,
+    //         numComponents,
+    //         type,
+    //         normalize,
+    //         stride,
+    //         offset);
+    //     gl.enableVertexAttribArray(
+    //         programInfo.attribLocations.vertexPosition);
+    // }
     
-        // Tell WebGL how to pull out the normals from
-    // the normal buffer into the vertexNormal attribute.
-    {
-        const numComponents = 3;
-        const type = gl.FLOAT;
-        const normalize = false;
-        const stride = 0;
-        const offset = 0;
-        gl.bindBuffer(gl.ARRAY_BUFFER, buffers_plane.normal);
+    //     // Tell WebGL how to pull out the normals from
+    // // the normal buffer into the vertexNormal attribute.
+    // {
+    //     const numComponents = 3;
+    //     const type = gl.FLOAT;
+    //     const normalize = false;
+    //     const stride = 0;
+    //     const offset = 0;
+    //     gl.bindBuffer(gl.ARRAY_BUFFER, buffers_plane.normal);
         
-        gl.vertexAttribPointer(
-            programInfo.attribLocations.vertexNormal,
-            numComponents,
-            type,
-            normalize,
-            stride,
-            offset);
+    //     gl.vertexAttribPointer(
+    //         programInfo.attribLocations.vertexNormal,
+    //         numComponents,
+    //         type,
+    //         normalize,
+    //         stride,
+    //         offset);
 
 
-        gl.enableVertexAttribArray(
-            programInfo.attribLocations.vertexNormal);
-    }
+    //     gl.enableVertexAttribArray(
+    //         programInfo.attribLocations.vertexNormal);
+    // }
 
 
-    {
-        const offset = 0;
-        const vertexCount = 4;
-        gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount);
-    }
+    // {
+    //     const offset = 0;
+    //     const vertexCount = 4;
+    //     gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount);
+    // }
   }
 
