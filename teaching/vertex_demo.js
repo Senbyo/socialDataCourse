098072973a4c5d2298a6c7ff0caac6bdf4b71b5c
Mark Bo Jensen <mboje@win.dtu.dk>
2020-01-30 16:43:17 +0100

    testing and updating

--- teaching/vertex_demo.js
+++ teaching/vertex_demo.js
@@ -43,7 +43,7 @@ function main() {
     // Apply lighting effect
 
     highp vec3 ambientLight = vec3(0.3, 0.3, 0.3);
-    highp vec3 directionalLightColor = vec3(1, 1, 1);
+    highp vec3 directionalLightColor = vec3(1.0, -2.0, 1.0);
     highp vec3 directionalVector = normalize(vec3(0.85, 0.8, 0.75));
 
     highp vec4 transformedNormal = uNormalMatrix * vec4(aVertexNormal, 1.0);
@@ -514,7 +514,7 @@ function drawScene(gl, programInfo, buffers, buffers_plane) {
             programInfo.attribLocations.vertexNormal);
     }
 
-    
+
     {
         const offset = 0;
         const vertexCount = 4;
