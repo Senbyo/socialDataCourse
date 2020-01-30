308f4ebe10a0d3ebcc129c6265e24678f4a96407
Mark Bo Jensen <mboje@win.dtu.dk>
2020-01-30 23:51:40 +0100

    updating and testin

--- teaching/vertex_projection_demo.js
+++ teaching/vertex_projection_demo.js
@@ -514,7 +514,7 @@ function drawScene(gl, programInfo, buffers, buffers_plane) {
         normalMatrix);
   
     if (document.getElementById("Red").value == 1.0){
-      const temp = parseFloat(document.getElementById("x"))
+      const temp = parseInt(document.getElementById("x"))
       const fieldOfView = 60 * Math.PI / 180;   // in radians
       const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
       const zNear = parseFloat(document.getElementById("y").value);
